59a5ede94e6860006d18fdba7d619a0455c8a701
Ali Ok <aliok@apache.org>
2014-10-11 23:11:21 +0200

    Line and dragging mouse cursor

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 // see http://stackoverflow.com/questions/10371539/why-define-anonymous-function-and-pass-jquery-as-the-argument
 // about passing $ as parameter
 (function ($) {
+    var EPSILON = 0.01;
+
     var Playground = function (canvasId) {
         var self = this;
         var $self = $(this);
@@ -23,12 +25,17 @@
 
         var components = [];       // TODO: make it a sorted set etc so that we have a z-index
 
+        //region Playground methods
         this.addComponent = function (component) {
             components.push(component);
             self.renderAll(canvasContext);
         };
 
         this.findPixelHit = function (x, y) {
+            // pixel hit priority is the element being dragged
+            if (state.dragging && state.elementBeingDragged && state.elementBeingDragged.containsPoint(x, y))
+                return state.elementBeingDragged;
+
             var i;
             for (i = 0; i < components.length; ++i) {
                 if (components[i].containsPoint(x, y)) {
@@ -46,7 +53,10 @@
                 components[i].render(canvasContext);
             }
         };
+        //endregion
 
+
+        //region Playground canvasEventHandlers
         var canvasEventHandlers = {
             mousedownHandler: function (e) {
                 var pixelHit = self.findPixelHit(e.offsetX, e.offsetY);
@@ -59,9 +69,11 @@
                     // 3. trigger 'mousedown' handler on playground
 
                     // but first set the playground state
-                    state.dragging = true;
-                    state.elementBeingDragged = pixelHit;
-                    state.elementDragOffset = pixelHit.offset(e.offsetX, e.offsetY);
+                    if (pixelHit.options.draggable) {
+                        state.dragging = true;
+                        state.elementBeingDragged = pixelHit;
+                        state.elementDragOffset = pixelHit.offset(e.offsetX, e.offsetY);
+                    }
 
                     $(pixelHit).trigger('mousedown');
                     $self.trigger('element:mousedown', {element: pixelHit});
@@ -117,11 +129,20 @@
                     $(pixelHit).trigger('mousemove');
                     $self.trigger('element:mousemove', {element: pixelHit});
                     $self.trigger('mousemove', {x: e.offsetX, y: e.offsetY});
+
+                    // set mouse cursor to move if pixelHit is draggable
+                    if (pixelHit.options.draggable) {
+                        $canvas.css('cursor', 'move');
+                    }
+
                 }
                 else {
                     // if we have don't have a component hitting that mouse mousemove
                     // do less stuff : just trigger mousemove handler on playground
                     $self.trigger('mousemove', {x: e.offsetX, y: e.offsetY});
+
+                    // set mouse cursor to default since we're not on any component anymore
+                    $canvas.css('cursor', 'default');
                 }
             },
 
@@ -151,12 +172,15 @@
         $canvas.on('mouseup', canvasEventHandlers.mouseupHandler);
         $canvas.on('mousemove', canvasEventHandlers.mousemoveHandler);
         $canvas.on('click', canvasEventHandlers.clickHandler);
+        //endregion
     };
 
 
+    //region BaseShape
     var BaseShape = function (options) {
         var defaultOptions =
         {
+            draggable: true,
             strokeWidth: 1,
             strokeColor: "#333",
             fillColor: "#333"
@@ -182,7 +206,9 @@
             throw "Not implemented : move";
         }
     };
+    //endregion
 
+    //region Rect
     var Rect = function (options) {
         var defaultOptions = {
             x: 0,
@@ -219,7 +245,60 @@
         }
     };
     Rect.prototype = $.extend({}, BaseShape.prototype, Rect.prototype);
+    //endregion
 
+    //region Line
+    var Line = function (options) {
+        var defaultOptions = {
+            x1: 0,
+            y1: 0,
+            x2: 10,
+            y2: 10
+        };
+        BaseShape.call(this, options);
+
+        $.extend(defaultOptions, this.options);
+        this.options = defaultOptions;
+    };
+
+    Line.prototype = {
+        containsPoint: function (px, py) {
+            if (px == this.options.x1 && py == this.options.y1)
+                return true;
+            if (px == this.options.x2 && py == this.options.y2)
+                return true;
+
+            var pxFromOrigin = px - this.options.x1;
+            var pyFromOrigin = py - this.options.y1;
+
+            if ((this._slope() * pxFromOrigin - pyFromOrigin) < EPSILON)
+                return true;
+        },
+        offset: function (x, y) {
+            return {x: x - this.options.x1, y: y - this.options.y1};
+        },
+        render: function (ctx) {
+            ctx.lineWidth = this.options.strokeWidth;
+            ctx.strokeStyle = this.options.strokeColor;
+            ctx.beginPath();
+            ctx.moveTo(this.options.x1, this.options.y1);
+            ctx.lineTo(this.options.x2, this.options.y2);
+            ctx.stroke();
+        },
+        move: function (x, y) {
+            this.options.x2 += x - this.options.x1;
+            this.options.y2 += y - this.options.y1;
+            this.options.x1 = x;
+            this.options.y1 = y;
+        },
+        _slope: function () {
+            if (this.options.x2 - this.options.x1 == 0)
+                return Number.MAX_VALUE;
+            return (this.options.y2 - this.options.y1) / (this.options.x2 - this.options.x1);
+        }
+    };
+    Line.prototype = $.extend({}, BaseShape.prototype, Line.prototype);
+    //endregion
 
     var playground = new Playground('c');
 //    $(playground).on('click', function(e, data){
@@ -231,12 +310,17 @@
 //    });
 
 
-    var rect = new Rect({x: 100, y: 200, w: 100, h: 100, "fillColor": "red"});
+    var rect = new Rect({x: 100, y: 200, w: 10, h: 10, "strokeColor": "#000", "fillColor": "transparent"});
+    var rect2 = new Rect({x: 100, y: 250, w: 10, h: 10, "strokeColor": "#000", "fillColor": "transparent"});
+    var line = new Line({x1: 100, y1: 250, x2: 150, y2: 300, "strokeColor": "#000", draggable: false});
+
 //    $(rect).on('click', function(e, data){
 //        console.log(e);
 //        console.log(data);
 //    });
 
     playground.addComponent(rect);
+    playground.addComponent(rect2);
+    playground.addComponent(line);
 
 })(jQuery);
